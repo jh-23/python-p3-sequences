efa67ac05eadedac194055ad3ab01846979e01fe
John Heger <heger7@gmail.com>
2024-01-29 20:48:12 -0600

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [] 
+    a, b = 0, 1
+    if (length == 0):
+        print([])
+    else:
+        for _ in range(length):
+            fibonacci_list.append(a)
+            a, b = b, a + b
+        print(fibonacci_list)
+            
+        
+print_fibonacci(length=8)
